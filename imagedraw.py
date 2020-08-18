6451a573f5f9c470ba46c9b3a376b6c1faa5f97b
faelee <69115680+faelee@users.noreply.github.com>
2020-08-17 22:29:31 -0400

    Calculate original box coordinates from ratios

---
+++ imagedraw.py
@@ -0,0 +1,36 @@
+from PIL import Image
+
+file = open(input("file name"))
+im = Image.open(input("image name"))
+width, height = im.size
+name = input("new name")
+
+def draw(word):
+    nums = [0.0] * 5
+    line = word.split()
+    for i in range(0, len(nums)):
+        nums[i] = float(line[i])
+    xmax = (2 * nums[1] + nums[3]) / 2 * width
+    xmin = (2 * nums[1] - nums[3]) / 2 * width
+    ymax = (2 * nums[2] + nums[4]) / 2 * height
+    ymin = (2 * nums[2] - nums[4]) / 2 * height
+    end = [round(xmin), round(ymin), round(xmax), round(ymax)]
+    return str(end)
+
+f = open(name + ".txt", "x")
+
+while True:
+
+    # Get next line from file 
+    line = file.readline()
+
+    # if line is empty 
+    # end of file is reached 
+    if not line:
+        break
+
+    out = draw(line)
+    f.write(out + "\n")
+
+file.close()
+
